3805899e6b166e0eac11ea8b58ac6965bf8f1b15
Cameron Taggart <cameron@tachyus.com>
2015-04-13 13:15:59 -0700

    prompt for the basic authentication if a 401

--- server.js
+++ server.js
@@ -8,10 +8,20 @@ var appInsights = new AppInsights();
 appInsights.trackAllHttpServerRequests("favicon");
 appInsights.trackAllUncaughtExceptions();
 http.createServer(function (req, res) {
+    if (req.url === '/') {
+        res.writeHead(302, { 'Location': 'http://ctaggart.github.io/SourceLink/' });
+        res.end();
+        return;
+    }
+    if (req.url === '/index2.txt') {
+        res.statusCode = 200;
+        res.end();
+        return;
+    }
     var agent = req.headers['user-agent'];
-    var user = req.headers['authorization'];
-    if (!user) {
-        res.statusCode = 401;
+    var auth = req.headers['authorization'];
+    if (!auth) {
+        res.writeHead(401, { 'WWW-Authenticate': 'Basic realm="SourceLink Proxy for GitHub"' });
         res.write('Authorization header required');
         res.end();
         return;
@@ -19,16 +29,25 @@ http.createServer(function (req, res) {
     var authReq = request({
         url: 'https://api.github.com/',
         // https://developer.github.com/v3/#user-agent-required
-        headers: { 'User-Agent': agent },
-        auth: { user: user }
+        headers: {
+            'User-Agent': agent,
+            'Authorization': auth
+        }
     }).on('response', function (authRsp) {
         if (authRsp.statusCode == 200) {
             var fileReq = request({
                 // https://developer.github.com/changes/2014-04-25-user-content-security/
                 url: 'https://raw.githubusercontent.com' + req.url,
-                headers: { 'User-Agent': agent },
-                auth: { user: user }
+                headers: {
+                    'User-Agent': agent,
+                    'Authorization': auth
+                }
             }).pipe(res);
+        }
+        else if (authRsp.statusCode == 401) {
+            res.writeHead(401, { 'WWW-Authenticate': 'Basic realm="SourceLink Proxy for GitHub"' });
+            res.write('Bad credentials');
+            res.end();
         }
         else {
             authReq.pipe(res);
--- server.ts
+++ server.ts
@@ -11,32 +11,50 @@ appInsights.trackAllHttpServerRequests("favicon");
 appInsights.trackAllUncaughtExceptions();
 
 http.createServer((req, res) => {
-  var agent = req.headers['user-agent'];
-  var user = req.headers['authorization'];
-  if(!user){
-    res.statusCode = 401
-    res.write('Authorization header required');
-    res.end();
-    return;
-  }
-  var authReq = request({
-    url: 'https://api.github.com/',
-    // https://developer.github.com/v3/#user-agent-required
-    headers: {'User-Agent': agent},
-    auth: {user: user}
-  })
-  .on('response', authRsp => {
-    if (authRsp.statusCode == 200) {
-      var fileReq = request({
-        // https://developer.github.com/changes/2014-04-25-user-content-security/
-        url: 'https://raw.githubusercontent.com' + req.url,
-        headers: {'User-Agent': agent},
-        auth: {user: user}
-      })
-      .pipe(res);
-    } else {
-      authReq.pipe(res);
+    if (req.url === '/') {
+        res.writeHead(302, { 'Location': 'http://ctaggart.github.io/SourceLink/' });
+        res.end();
+        return;
     }
-  });
-  authReq.end();
+    if (req.url === '/index2.txt') {
+        res.statusCode = 200;
+        res.end();
+        return;
+    }
+    var agent = req.headers['user-agent'];
+    var auth = req.headers['authorization'];
+    if (!auth) {
+        res.writeHead(401, { 'WWW-Authenticate': 'Basic realm="SourceLink Proxy for GitHub"' });
+        res.write('Authorization header required');
+        res.end();
+        return;
+    }
+    var authReq = request({
+        url: 'https://api.github.com/',
+        // https://developer.github.com/v3/#user-agent-required
+        headers: {
+            'User-Agent': agent,
+            'Authorization': auth
+        }
+    })
+    .on('response', authRsp => {
+        if (authRsp.statusCode == 200) {
+            var fileReq = request({
+                // https://developer.github.com/changes/2014-04-25-user-content-security/
+                url: 'https://raw.githubusercontent.com' + req.url,
+                headers: {
+                    'User-Agent': agent,
+                    'Authorization': auth
+                }
+            })
+            .pipe(res);
+        } else if (authRsp.statusCode == 401) {
+            res.writeHead(401, { 'WWW-Authenticate': 'Basic realm="SourceLink Proxy for GitHub"' });
+            res.write('Bad credentials');
+            res.end();
+        } else {
+            authReq.pipe(res);
+        }
+    });
+    authReq.end();
 }).listen(process.env.PORT || 3000);
